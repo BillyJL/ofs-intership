0e4618007eee0fc3197c11618f66bfd194af08de
Taras <tarasmikulskyi@ukr.net>
2023-08-11 13:02:50 +0300

    Created footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import Header from './components/header/Header.jsx';
+import Footer from './components/footer/Footer.jsx';
 import './assets/styles/globals.scss';
 
 const App = () => {
-  return (
-    <Header />
-  );
-}
+	return (
+		<>
+			<Header />
+			<Footer />
+		</>
+	);
+};
 
 export default App;
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import CategoryList from '../common/categoryList/CategoryList';
+import Contacts from './components/contacts/Contact';
+import Copyright from './components/copyright/Copyright';
+import Icons from './components/Icons/Icons';
+import './Footer.scss'
+
+const Footer = () => {
+	const firstList = ['Alcohol', 'Art', 'Books', 'Drink', 'Electronics'];
+	const secondList = ['Home', 'Jewelry', 'Kids & Baby', 'Men`s Fashion', 'Mobile', 'Motocycles', 'Movies', 'Music'];
+	const thirdList = ['Sport', 'Toys', 'Travel', 'Women`s Fashion'];
+	const fourthList = ['About Us', 'Delivery', 'Testimonials', 'Contact'];
+
+	return (
+		<footer className='footer'>
+			<Copyright className='div1' />
+			<Contacts className='div5' />
+			<h3 className='column-name div2'>Categories</h3>
+			<CategoryList className='div6 text' listOfCategories={firstList} />
+			<CategoryList className='div7 text' listOfCategories={secondList} />
+			<CategoryList className='div8 text' listOfCategories={thirdList} />
+			<h3 className='column-name div3'>About</h3>
+			<CategoryList className='div9 text' listOfCategories={fourthList} />
+			<Icons className='div4' />
+		</footer>
+	)
+}
+
+export default Footer;
