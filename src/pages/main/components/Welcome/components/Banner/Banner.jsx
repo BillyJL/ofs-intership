c3a6462ac126d8f0f8f5ab41000dc24afa324930
Taras <tarasmikulskyi@ukr.net>
2023-08-13 16:50:43 +0300

    Created banner component

---
+++ src/pages/main/components/Welcome/components/Banner/Banner.jsx
@@ -0,0 +1,18 @@
+import Sales from './components/Sales/Sales';
+import './Banner.scss';
+import Button from '../../../../../../components/common/Button/Button';
+
+const Banner = () => {
+	return (
+		<div className="banner div2">
+			<Sales />
+			<div className='banner-text'>
+				<h3 className='headline'>Follow us on Facebook</h3>
+				<p className='text'>Sed ut perspiciatis unde omnis iste natus error sit</p>
+			</div>
+			<Button className={'banner-button'} text={'follow'} />
+		</div>
+	)
+}
+
+export default Banner;
