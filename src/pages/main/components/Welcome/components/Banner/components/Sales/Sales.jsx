b3dfe55a8d2c454b7f7b97de5e0673906643ebd1
Taras <tarasmikulskyi@ukr.net>
2023-08-13 16:50:05 +0300

    Created sales component

---
+++ src/pages/main/components/Welcome/components/Banner/components/Sales/Sales.jsx
@@ -0,0 +1,12 @@
+import './Sales.scss';
+
+const Sales = () => {
+	return (
+		<div className="sales">
+			<h2 className='percent'>55%</h2>
+			<p className='sales-text'>Summer <br /> sales</p>
+		</div>
+	);
+};
+
+export default Sales;
