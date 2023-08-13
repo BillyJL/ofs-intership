536fab935076be15e6c2796f433cb7316693dc8d
Taras <tarasmikulskyi@ukr.net>
2023-08-13 14:47:14 +0300

    Created carousel component

---
+++ src/pages/main/components/Welcome/components/Slider/components/Carousel/Carousel.jsx
@@ -0,0 +1,18 @@
+import useEmblaCarousel from 'embla-carousel-react';
+
+import Autoplay from 'embla-carousel-autoplay';
+import Dots from './components/Dots/Dots';
+import Container from './components/Container/Container';
+
+const Carousel = ({ slides, options }) => {
+	const [emblaRef, emblaApi] = useEmblaCarousel(options, [Autoplay()]);
+
+	return (
+		<div className="embla div1">
+			<Container emblaRef={emblaRef} slides={slides} />
+			<Dots emblaApi={emblaApi} />
+		</div>
+	);
+};
+
+export default Carousel;
