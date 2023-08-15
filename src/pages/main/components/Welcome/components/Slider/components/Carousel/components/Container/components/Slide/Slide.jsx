ade87e099fa735d2c5d677717f841f9105416f75
Taras <tarasmikulskyi@ukr.net>
2023-08-15 13:06:38 +0300

    Changed slide structure

--- src/pages/main/components/Welcome/components/Slider/components/Carousel/components/Container/components/Slide/Slide.jsx
+++ src/pages/main/components/Welcome/components/Slider/components/Carousel/components/Container/components/Slide/Slide.jsx
@@ -1,9 +1,9 @@
 import Button from '../../../../../../../../../../../../components/common/Button/Button';
 import './Slide.scss';
 
-const Slide = ({ headline, text }) => {
+const Slide = ({ headline, text, img, textAlign }) => {
 	return (
-		<div className="embla__slide slide">
+		<div className="embla__slide slide" style={{textAlign, backgroundImage: `url(${img})`}}>
 			<h1 className="headline">{headline}</h1>
 			<p className="slide-text">{text}</p>
 			<Button className="slide-button" text={'view more'} />
