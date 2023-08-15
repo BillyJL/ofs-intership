60bb34536a6ad19c008206c186bb1d9e68d733b5
Taras <tarasmikulskyi@ukr.net>
2023-08-15 11:29:20 +0300

    Created client config

---
+++ src/config/client.js
@@ -0,0 +1,22 @@
+import delivery from '../assets/images/Delivery_Packages.png';
+import triangular from '../assets/images/Triangular_Arrows.png';
+import clipboard from '../assets/images/Clipboard_Verification.png';
+
+
+export const slogansList = [
+	{
+		headline: 'Focus',
+		text: 'Our unwavering focus on superior service delivery and building next generation competencies',
+		img: delivery,
+	},
+	{
+		headline: 'method',
+		text: 'A standardized methodology designed to deliver measurable business results and predictable costs',
+		img: triangular,
+	},
+	{
+		headline: 'Knowledge',
+		text: 'A highly skilled, proactive workforce that reliably improves each client’s ROI while mitigating their business risk',
+		img: clipboard,
+	},
+];
