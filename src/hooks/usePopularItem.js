cd0c6c2072e5a83fa3551e5b3523fa15be3821aa
Taras <tarasmikulskyi@ukr.net>
2023-08-17 19:35:33 +0300

    Deleted console log and useEffect dependencies

--- src/hooks/usePopularItem.js
+++ src/hooks/usePopularItem.js
@@ -6,11 +6,9 @@ export const usePopularItem = (setCardItems) => {
 	const { server } = clientConfig;
 	const {request} = useHttp();
 	const serverUrl = `${server.domain}${server.endpoints.request}`;
-	console.log(serverUrl);
 	useEffect(() => {
 		request(serverUrl).then((response) => {
-			console.log(response);
 			setCardItems(response);
 		});
-	}, [request, serverUrl]);
+	}, []);
 };
