44a53302e376899595e4a40e736561ecbf178653
Taras <tarasmikulskyi@ukr.net>
2023-08-17 15:02:34 +0300

    Created useHttp hook

---
+++ src/hooks/common/useHttp.js
@@ -0,0 +1,22 @@
+export const useHttp = () => {
+	const request = async (
+		url,
+		method = 'GET',
+		body = null,
+		headers = { 'Content-Type': 'application/json' },
+	) => {
+		const response = await fetch(url, { method, body, headers });
+
+		if (!response.ok) {
+			throw new Error(
+				`Could not fetch ${url}, status: ${response.status}`,
+			);
+		}
+
+		return response.json();
+	};
+
+	return {
+		request,
+	};
+};
