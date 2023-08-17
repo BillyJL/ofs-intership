cd90493e429437f855485b04edfa55ef7c5007cf
Taras <tarasmikulskyi@ukr.net>
2023-08-17 14:53:01 +0300

    Changed card component for universal data

--- src/pages/main/components/PopularItems/components/Card/Card.jsx
+++ src/pages/main/components/PopularItems/components/Card/Card.jsx
@@ -1,21 +1,18 @@
-import img from '@images/popular-item.png';
 import plusIcon from '@images/Plus.png';
 import heartIcon from '@images/Heart.png';
 import './Card.scss';
 
-const Card = () => {
+const Card = ({ name, price, img }) => {
 	return (
 		<div className="card">
-			<img className="card-image" src={img} alt="" />
-			<h3 className="card-name">
-				Kristina Dam Oak Table With White Marble Top
-			</h3>
-			<p className="card-price">$ 799.55</p>
-			<div className='card-gradient'>
-				<button className='card-gradient-button'>
+			<img className="card-image" src={img} alt="item image" />
+			<h3 className="card-name">{name}</h3>
+			<p className="card-price">{price}</p>
+			<div className="card-gradient">
+				<button className="card-gradient-button">
 					<img src={plusIcon} alt="Plus Icon" />
 				</button>
-				<button className='card-gradient-button'>
+				<button className="card-gradient-button">
 					<img src={heartIcon} alt="Heart Icon" />
 				</button>
 			</div>
