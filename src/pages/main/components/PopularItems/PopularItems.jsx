fa56147a2a4bb88df7d15a05b3ff68ab2f22b607
Taras <tarasmikulskyi@ukr.net>
2023-08-17 15:10:50 +0300

    Created popular items component

--- src/pages/main/components/PopularItems/PopularItems.jsx
+++ src/pages/main/components/PopularItems/PopularItems.jsx
@@ -1,9 +1,17 @@
-import './PopularItems.scss';
+import { useState } from 'react';
+import { usePopularItem } from 'hooks/usePopularItem';
 import Card from './components/Card/Card';
+import Cards from './components/Cards/Cards';
+import CardWithButton from './components/CardWithButton/CardWithButton';
+import Message from './components/Message/Message';
+import Button from 'components/common/Button/Button';
+import './PopularItems.scss';
 
 const PopularItems = () => {
 	const [cardItems, setCardItems] = useState([]);
 	const [isLoadedCards, setIsLoadedCards] = useState(false);
+	usePopularItem(setCardItems);
+
 	return (
 		<div className="popularItems">
 			<div class="headline">
@@ -11,13 +19,20 @@ const PopularItems = () => {
 				<span class="line-text">Popular Items</span>
 				<span class="line"></span>
 			</div>
-			<div className='items-wrapper'>
-				<Card />
-				<Card />
-				<Card />
-				<Card />
-				<Card />
+			<div className="items-wrapper">
+				<Card {...cardItems[0]} />
+				<CardWithButton />
+				<Cards items={cardItems.slice(1, 6)} />
+				<Message />
+				{isLoadedCards && <Cards items={cardItems.slice(6)} />}
 			</div>
+			{isLoadedCards || (
+				<Button
+					onClick={() => setIsLoadedCards(true)}
+					text={'load more'}
+					className={'load'}
+				/>
+			)}
 		</div>
 	);
 };
