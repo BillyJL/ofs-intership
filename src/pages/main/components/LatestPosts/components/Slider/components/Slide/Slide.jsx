fb6d8e156e0e9384098f465a78c09d2514f9ad29
Taras <tarasmikulskyi@ukr.net>
2023-08-18 14:12:34 +0300

    Deleted console log

--- src/pages/main/components/LatestPosts/components/Slider/components/Slide/Slide.jsx
+++ src/pages/main/components/LatestPosts/components/Slider/components/Slide/Slide.jsx
@@ -1,7 +1,6 @@
 import './Slide.scss';
 
 const Slide = ({ headline, theme, image }) => {
-	console.log(headline, theme, image);
 	return (
 		<div className="embla__slide slider-slide">
 			<div className='slider-slide-content'>
