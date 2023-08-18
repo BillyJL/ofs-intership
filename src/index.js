71fd357ffa4098bb289e9cc88e6caaa42a6fd7b6
Taras <tarasmikulskyi@ukr.net>
2023-08-18 14:23:51 +0300

    Added provider to use store

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import { Provider } from 'react-redux';
+import { setupStore } from 'store/index.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const store = setupStore();
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Provider store={store}>
+		<App />
+	</Provider>,
 );
