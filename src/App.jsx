a438d6f97b6d3e426d0adeb3ff07c479d1b4171c
Taras <tarasmikulskyi@ukr.net>
2023-08-21 15:26:31 +0300

    Created routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import MainPage from '@pages/main/MainPage.jsx';
 import '@assets/styles/globals.scss';
+import NotFound from '@pages/NotFound/NotFound';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 const App = () => {
-
+	const router = createBrowserRouter([
+		{
+			path: '/',
+			element: <MainPage />,
+		},
+		{
+			path: '/404',
+			element: <NotFound />,
+			breadcrumb: "Custom Example"
+		}
+	]);
 	return (
-		<MainPage />
+		<RouterProvider router={router} />
 	);
 };
 
