193e4c31a1af7e9e95a965a99cb5926b0888e0c7
Taras <tarasmikulskyi@ukr.net>
2023-08-22 15:45:10 +0300

    Created router config

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import MainPage from '@pages/main/MainPage.jsx';
 import '@assets/styles/globals.scss';
-import NotFound from '@pages/NotFound/NotFound';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { router } from 'config/router';
+import { RouterProvider } from 'react-router-dom';
 
 const App = () => {
-	const router = createBrowserRouter([
-		{
-			path: '/',
-			element: <MainPage />,
-		},
-		{
-			path: '/404',
-			element: <NotFound />,
-			breadcrumb: "Custom Example"
-		}
-	]);
+
 	return (
 		<RouterProvider router={router} />
 	);
