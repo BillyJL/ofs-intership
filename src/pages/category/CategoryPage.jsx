3e90d9657d4337c6420da5bcc3e37ceb34c085af
Taras <tarasmikulskyi@ukr.net>
2023-08-23 12:56:01 +0300

    Finished category page

--- src/pages/category/CategoryPage.jsx
+++ src/pages/category/CategoryPage.jsx
@@ -1,12 +1,19 @@
 import { withBreadcrumbsLayout } from 'components/layouts';
 import './CategoryPage.scss';
 import Filters from './components/Filters/Filters';
+import ItemsList from './components/ItemsList/ItemsList';
+import LatestPosts from 'components/common/LatestPosts/LatestPosts';
 
 const CategoryPage = () => {
 	return (
-		<div className="category">
-			<Filters />
-		</div>
+		<>
+			<div className="category">
+				<Filters />
+				<h4 className="results-num">12,931 results in apparel</h4>
+				<ItemsList />
+			</div>
+			<LatestPosts />
+		</>
 	);
 };
 
