22a364392988f12c8c175d842b7351f835027dde
Taras <tarasmikulskyi@ukr.net>
2023-08-23 11:28:56 +0300

    Created dropdown component in for filter

---
+++ src/pages/category/components/Filters/components/Filter/components/Dropdown/Dropdown.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import './Dropdown.scss';
+
+const Dropdown = ({ options }) => {
+	const [selectedOption, setSelectedOption] = useState(null);
+	const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+	const handleOptionClick = (option) => {
+		setSelectedOption(option);
+		setIsDropdownOpen(false);
+	};
+
+	return (
+		<div className="dropdown">
+			<div
+				className={`dropdown-toggle ${isDropdownOpen ? 'open' : ''}`}
+				onClick={() => setIsDropdownOpen(!isDropdownOpen)}
+			>
+				{selectedOption || 'Select an option'}
+				<span className="triangle"></span>
+			</div>
+			{isDropdownOpen && (
+				<ul className="dropdown-options">
+					{options.map((option) => (
+						<li
+							key={option}
+							className={`option ${
+								selectedOption === option ? 'selected' : ''
+							}`}
+							onClick={() => handleOptionClick(option)}
+						>
+							{option}
+						</li>
+					))}
+				</ul>
+			)}
+		</div>
+	);
+};
+
+export default Dropdown;
