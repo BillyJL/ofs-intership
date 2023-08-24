b197a371627a591afb67b6728da818f2493ac3eb
Taras <tarasmikulskyi@ukr.net>
2023-08-24 17:25:58 +0300

    Added link to product page

--- src/components/common/LatestPosts/components/Slider/components/Slide/Slide.jsx
+++ src/components/common/LatestPosts/components/Slider/components/Slide/Slide.jsx
@@ -1,11 +1,18 @@
+import { Link } from 'react-router-dom';
 import './Slide.scss';
 
 const Slide = ({ headline, theme, image }) => {
 	return (
 		<div className="embla__slide slider-slide">
-			<div className='slider-slide-content'>
-				<img className="slider-slide-image" src={image} alt="headline" />
-				<h3 className="slider-slide-headline">{headline}</h3>
+			<div className="slider-slide-content">
+				<Link to="/product/pr1">
+					<img
+						className="slider-slide-image"
+						src={image}
+						alt="headline"
+					/>
+					<h3 className="slider-slide-headline">{headline}</h3>
+				</Link>
 				<p className="slider-slide-theme">{theme}</p>
 			</div>
 		</div>
--- src/pages/main/components/PopularItems/components/CardWithButton/CardWithButton.jsx
+++ src/pages/main/components/PopularItems/components/CardWithButton/CardWithButton.jsx
@@ -2,6 +2,7 @@ import image from '@images/Layer_152.png';
 import './CardWithButton.scss';
 import { cartSlice } from 'store/reducers/CartSlice';
 import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const CardWithButton = () => {
 	const { numOfProductIncrement } = cartSlice.actions;
@@ -9,11 +10,21 @@ const CardWithButton = () => {
 	return (
 		<div className="button-card">
 			<div className="button-card-content">
-				<img className="button-card-image" src={image} alt="item image" />
-				<h3 className="button-card-name">Hay - About A Lounge Chair AAL 93</h3>
-				<div className='multi-button'>
-					<span>$659.55</span>
-					<button onClick={() => dispatch(numOfProductIncrement(1))}>Buy now</button>
+				<Link to="/product/pr1">
+					<img
+						className="button-card-image"
+						src={image}
+						alt="item image"
+					/>
+					<h3 className="button-card-name">
+						Hay - About A Lounge Chair AAL 93
+					</h3>
+				</Link>
+				<div className="multi-button">
+					<Link to="/product/pr1"><span>$659.55</span></Link>
+					<button onClick={() => dispatch(numOfProductIncrement(1))}>
+						Buy now
+					</button>
 				</div>
 			</div>
 		</div>
