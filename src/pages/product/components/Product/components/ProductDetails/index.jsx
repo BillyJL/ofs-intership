9b47d62beac12fc399dead51b7424ae5e2169352
Taras <tarasmikulskyi@ukr.net>
2023-08-24 17:21:11 +0300

    Created product details component

---
+++ src/pages/product/components/Product/components/ProductDetails/index.jsx
@@ -0,0 +1,28 @@
+import Icons from 'components/common/Icons/Icons';
+import './ProductDetails.scss';
+import DropdownImitation from './components/DropdownImitation';
+import QuantityCounter from './components/QuantityCounter';
+import ReadMoreText from './components/ReadMoreText';
+import { clientConfig } from 'config/client';
+
+
+const ProductDetails = () => {
+	const {product: {text}} = clientConfig
+
+	return (
+		<div className="details">
+			<h2 className="details-price">$ 299.99</h2>
+			<div className="details-choose">
+				<DropdownImitation />
+				<QuantityCounter />
+			</div>
+			<ReadMoreText text={text} />
+			<div className='details-social'>
+				<span>Share</span>
+				<Icons />
+			</div>
+		</div>
+	);
+};
+
+export default ProductDetails;
