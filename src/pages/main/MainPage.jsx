de35e9de07f8fbe5bd07a3a7aaeede8ea986ac26
Taras <tarasmikulskyi@ukr.net>
2023-08-24 18:54:48 +0300

    Changed component name from Welcome to index

--- src/pages/main/MainPage.jsx
+++ src/pages/main/MainPage.jsx
@@ -1,6 +1,6 @@
 import { withMainLayout } from 'components/layouts';
 import Services from 'components/common/Services';
-import Welcome from './components/Welcome/Welcome';
+import Welcome from './components/Welcome';
 import Banner from './components/Banner';
 import PopularItems from './components/PopularItems';
 import LatestPosts from '@components/common/LatestPosts';
