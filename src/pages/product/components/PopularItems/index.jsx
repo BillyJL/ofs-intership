704ba332bab54e55ce03515572eec7fb28e0a77f
Taras <tarasmikulskyi@ukr.net>
2023-08-24 17:25:00 +0300

    Created popular items component for product page

---
+++ src/pages/product/components/PopularItems/index.jsx
@@ -0,0 +1,21 @@
+import './PopularItems.scss';
+import { usePopularItem } from 'hooks/usePopularItem';
+import Cards from 'components/common/Cards/Cards';
+
+const PopularItems = () => {
+	const { cardItems } = usePopularItem();
+	return (
+		<div className="popular-items">
+			<div class="headline">
+				<span class="line"></span>
+				<span class="line-text">Popular Items</span>
+				<span class="line"></span>
+			</div>
+			<div className="items-wrapper">
+				<Cards items={cardItems.slice(0, 4)} />
+			</div>
+		</div>
+	)
+}
+
+export default PopularItems;
