0385a06e99e3141a260a40f96b4a4354cbd1ee16
Taras <tarasmikulskyi@ukr.net>
2023-08-24 17:12:25 +0300

    Created component dropdown imitation

---
+++ src/pages/product/components/Product/components/ProductDetails/components/DropdownImitation/index.jsx
@@ -0,0 +1,13 @@
+import './DropdownImitation.scss';
+
+const DropdownImitation = () => {
+	return (
+		<div className="dropdown-imitation">
+			<div className="circle"></div>
+			<span className="text">Dark grey</span>
+			<div className="triangle"></div>
+		</div>
+	);
+};
+
+export default DropdownImitation;
