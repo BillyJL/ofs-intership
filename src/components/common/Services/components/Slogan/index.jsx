ed0cee356b9a317ce92822e62ac1561eeea09214
Taras <tarasmikulskyi@ukr.net>
2023-08-25 19:18:24 +0300

    Added adaptation to Slogan

--- src/components/common/Services/components/Slogan/index.jsx
+++ src/components/common/Services/components/Slogan/index.jsx
@@ -3,9 +3,11 @@ import './Slogan.scss';
 const Slogan = ({ headline, text, img }) => {
 	return (
 		<div className="slogan">
-			<img className="slogan-image div1" src={img} alt="" />
-			<h3 className="slogan-headline div2">{headline}</h3>
-			<p className="slogan-text div3">{text}</p>
+			<img className="slogan-image" src={img} alt="" />
+			<div className="slogan-main">
+				<h3 className="slogan-main-headline">{headline}</h3>
+				<p className="slogan-main-text">{text}</p>
+			</div>
 		</div>
 	);
 };
