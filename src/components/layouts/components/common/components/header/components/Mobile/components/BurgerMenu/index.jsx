2fd2f5d7e0d95a13d033169abae4c2413f51510b
Taras <tarasmikulskyi@ukr.net>
2023-08-26 17:19:07 +0300

    Created BurgerMenu component

---
+++ src/components/layouts/components/common/components/header/components/Mobile/components/BurgerMenu/index.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+import './BurgerMenu.scss';
+import Dropdown from 'components/common/Dropdown';
+import CategoryList from 'components/common/CategoryList';
+import { clientConfig } from 'config/client';
+
+const BurgerMenu = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+	const {
+		header: { firstList, fourthList },
+	} = clientConfig;
+
+	return (
+		<div className="burger-menu">
+			<div
+				className={`menu-icon ${isMenuOpen ? 'open' : ''}`}
+				onClick={() => setIsMenuOpen(!isMenuOpen)}
+			>
+				<span className="bar"></span>
+				<span className="bar"></span>
+				<span className="bar"></span>
+			</div>
+			{isMenuOpen && (
+				<div className="menu-content">
+					<Dropdown label="Services" className='menu-content-dropdown'>
+						<Dropdown label="Product Categories" className='child-dropdown'>
+							<CategoryList listOfCategories={firstList} className='dropdown-list' />
+						</Dropdown>
+						<Dropdown label="Sales" className='child-dropdown'>
+							<CategoryList listOfCategories={fourthList} className='dropdown-list' />
+						</Dropdown>
+					</Dropdown>
+					<Dropdown label="Company" className='menu-content-dropdown' />
+					<Dropdown label="Library" className='menu-content-dropdown' />
+					<Dropdown label="Contact US" className='menu-content-dropdown' />
+				</div>
+			)}
+		</div>
+	);
+};
+
+export default BurgerMenu;
