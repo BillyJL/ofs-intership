74918f4eb6a2b5dd05d175eb0ef665e265e738d1
Taras <tarasmikulskyi@ukr.net>
2023-08-26 13:48:27 +0300

    Changed mobile adaptation

--- src/components/layouts/components/common/components/footer/components/Mobile/index.jsx
+++ src/components/layouts/components/common/components/footer/components/Mobile/index.jsx
@@ -5,6 +5,7 @@ import './Mobile.scss';
 import Dropdown from './components/Dropdown';
 import { clientConfig } from 'config/client';
 import Icons from 'components/common/Icons';
+import IconBar from 'components/common/IconBar';
 
 const Mobile = () => {
 	const {
@@ -15,30 +16,34 @@ const Mobile = () => {
 	} = clientConfig;
 	return (
 		<div className="mobile">
-			<Copyright />
-			<Dropdown label="contact">
-				<Address />
-				<p>
-					<a href="mailto:contact@osf-global.com">
-						contact@osf-global.com
-					</a>
-				</p>
-				<p>
-					<a href="tel:+18885484344">+1 (888) 548-4344</a>
-				</p>
-			</Dropdown>
-			<Dropdown label="category">
-				<CategoryList
-					listOfCategories={firstList.concat(secondList, thirdList)}
-				/>
-			</Dropdown>
-			<Dropdown label="about">
-				<CategoryList
-					listOfCategories={fourthList}
-				/>
-			</Dropdown>
-			<div className='icon-wrapper'>
+			<div className="mobile-content">
+				<Copyright />
+				<Dropdown label="contact">
+					<Address />
+					<p>
+						<a href="mailto:contact@osf-global.com">
+							contact@osf-global.com
+						</a>
+					</p>
+					<p>
+						<a href="tel:+18885484344">+1 (888) 548-4344</a>
+					</p>
+				</Dropdown>
+				<Dropdown label="category">
+					<CategoryList
+						listOfCategories={firstList.concat(
+							secondList,
+							thirdList,
+						)}
+					/>
+				</Dropdown>
+				<Dropdown label="about">
+					<CategoryList listOfCategories={fourthList} />
+				</Dropdown>
+			</div>
+			<div className="icon-wrapper">
 				<Icons />
+				<IconBar />
 			</div>
 		</div>
 	);
