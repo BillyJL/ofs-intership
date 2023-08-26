5591af6cc7d63f2f05c77902a78a8786f7d5fd74
Taras <tarasmikulskyi@ukr.net>
2023-08-26 17:29:04 +0300

    Added image to navigation in BurgerMenu

--- src/components/layouts/components/common/components/header/components/Mobile/components/BurgerMenu/index.jsx
+++ src/components/layouts/components/common/components/header/components/Mobile/components/BurgerMenu/index.jsx
@@ -3,6 +3,7 @@ import './BurgerMenu.scss';
 import Dropdown from 'components/common/Dropdown';
 import CategoryList from 'components/common/CategoryList';
 import { clientConfig } from 'config/client';
+import navImage from '@images/nav-photo.png';
 
 const BurgerMenu = () => {
 	const [isMenuOpen, setIsMenuOpen] = useState(false);
@@ -22,17 +23,39 @@ const BurgerMenu = () => {
 			</div>
 			{isMenuOpen && (
 				<div className="menu-content">
-					<Dropdown label="Services" className='menu-content-dropdown'>
-						<Dropdown label="Product Categories" className='child-dropdown'>
-							<CategoryList listOfCategories={firstList} className='dropdown-list' />
+					<Dropdown
+						label="Services"
+						className="menu-content-dropdown"
+					>
+						<Dropdown
+							label="Product Categories"
+							className="child-dropdown"
+						>
+							<CategoryList
+								listOfCategories={firstList}
+								className="dropdown-list"
+							/>
 						</Dropdown>
-						<Dropdown label="Sales" className='child-dropdown'>
-							<CategoryList listOfCategories={fourthList} className='dropdown-list' />
+						<Dropdown label="Sales" className="child-dropdown">
+							<CategoryList
+								listOfCategories={fourthList}
+								className="dropdown-list"
+							/>
 						</Dropdown>
 					</Dropdown>
-					<Dropdown label="Company" className='menu-content-dropdown' />
-					<Dropdown label="Library" className='menu-content-dropdown' />
-					<Dropdown label="Contact US" className='menu-content-dropdown' />
+					<Dropdown
+						label="Company"
+						className="menu-content-dropdown"
+					/>
+					<Dropdown
+						label="Library"
+						className="menu-content-dropdown"
+					/>
+					<Dropdown
+						label="Contact US"
+						className="menu-content-dropdown"
+					/>
+					<img src={navImage} alt='naw photo' />
 				</div>
 			)}
 		</div>
