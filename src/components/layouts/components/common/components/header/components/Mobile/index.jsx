c6773e144ca040ad34a215ec6f170a2e32eeb085
Taras <tarasmikulskyi@ukr.net>
2023-08-26 17:19:30 +0300

    Finished mobile header

--- src/components/layouts/components/common/components/header/components/Mobile/index.jsx
+++ src/components/layouts/components/common/components/header/components/Mobile/index.jsx
@@ -1,11 +1,13 @@
 import { Link } from 'react-router-dom';
 import './Mobile.scss';
 import logo from '@images/logo.png'
+import BurgerMenu from './components/BurgerMenu';
 
 const Mobile = () => {
 	return (
 		<div className='header-mobile'>
 			<Link to='/'><img src={logo} alt="logo" /></Link>
+			<BurgerMenu />
 		</div>
 	)
 }
