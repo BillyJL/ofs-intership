3326644afd48971968a8e65d791211613c312d3b
Taras <tarasmikulskyi@ukr.net>
2023-08-26 15:32:46 +0300

    Renamed footer mobile scss classes

--- src/components/layouts/components/common/components/footer/components/Mobile/index.jsx
+++ src/components/layouts/components/common/components/footer/components/Mobile/index.jsx
@@ -2,7 +2,7 @@ import CategoryList from 'components/common/CategoryList';
 import Address from '../common/Address';
 import Copyright from '../common/Copyright';
 import './Mobile.scss';
-import Dropdown from './components/Dropdown';
+import Dropdown from '../../../../../../../common/Dropdown';
 import { clientConfig } from 'config/client';
 import Icons from 'components/common/Icons';
 import IconBar from 'components/common/IconBar';
@@ -15,32 +15,27 @@ const Mobile = () => {
 		},
 	} = clientConfig;
 	return (
-		<div className="mobile">
-			<div className="mobile-content">
-				<Copyright />
-				<Dropdown label="contact">
-					<Address />
-					<p>
-						<a href="mailto:contact@osf-global.com">
-							contact@osf-global.com
-						</a>
-					</p>
-					<p>
-						<a href="tel:+18885484344">+1 (888) 548-4344</a>
-					</p>
-				</Dropdown>
-				<Dropdown label="category">
-					<CategoryList
-						listOfCategories={firstList.concat(
-							secondList,
-							thirdList,
-						)}
-					/>
-				</Dropdown>
-				<Dropdown label="about">
-					<CategoryList listOfCategories={fourthList} />
-				</Dropdown>
-			</div>
+		<div className="footer-mobile">
+			<Copyright />
+			<Dropdown label="contact">
+				<Address />
+				<p>
+					<a href="mailto:contact@osf-global.com">
+						contact@osf-global.com
+					</a>
+				</p>
+				<p>
+					<a href="tel:+18885484344">+1 (888) 548-4344</a>
+				</p>
+			</Dropdown>
+			<Dropdown label="category">
+				<CategoryList
+					listOfCategories={firstList.concat(secondList, thirdList)}
+				/>
+			</Dropdown>
+			<Dropdown label="about">
+				<CategoryList listOfCategories={fourthList} />
+			</Dropdown>
 			<div className="icon-wrapper">
 				<Icons />
 				<IconBar />
