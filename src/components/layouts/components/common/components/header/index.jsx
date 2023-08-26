7d422c796fd338112e22ec1c10a95bd391bd2c43
Taras <tarasmikulskyi@ukr.net>
2023-08-26 13:49:31 +0300

    Changed paths

--- src/components/layouts/components/common/components/header/index.jsx
+++ src/components/layouts/components/common/components/header/index.jsx
@@ -1,7 +1,7 @@
 import Logo from './components/Logo/index.jsx';
 import Navigation from './components/Navigation/index.jsx';
 import Dropdowns from './components/Dropdowns/index.jsx';
-import IconBar from './components/IconBar/index.jsx';
+import IconBar from 'components/common/IconBar/index.jsx';
 import { Link } from 'react-router-dom';
 
 import './Header.scss'
