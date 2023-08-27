c70f6cf8e626b6c1e5e70370dfd39083ec0c1e85
Taras <tarasmikulskyi@ukr.net>
2023-08-27 16:16:04 +0300

    Added adaptation for Filters

--- src/pages/category/components/Filters/index.jsx
+++ src/pages/category/components/Filters/index.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react';
 import { clientConfig } from 'config/client';
 import './Filters.scss';
 import Filter from './components/Filter';
 
 const Filters = () => {
+	const [showFilters, setShowFilters] = useState(true);
 	const {
 		category: { filters },
 	} = clientConfig;
 	return (
-		<div className="filter">
-			{filters.map((filter) => (
-				<Filter {...filter} />
-			))}
+		<div className="filter-wrapper">
+			<span
+				onClick={() => setShowFilters(!showFilters)}
+				className="filter-hide"
+			>
+				{showFilters ? 'Hide Filters' : 'Show Filters'}
+			</span>
+			{showFilters && (
+				<div className="filter">
+					{filters.map((filter) => (
+						<Filter {...filter} />
+					))}
+				</div>
+			)}
 		</div>
 	);
 };
