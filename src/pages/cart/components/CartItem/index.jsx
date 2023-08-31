51ce1ce1666dcc5241092c8affb613791736c021
Taras <tarasmikulskyi@ukr.net>
2023-08-31 13:07:41 +0300

    Added dynamism to the  CartItem

--- src/pages/cart/components/CartItem/index.jsx
+++ src/pages/cart/components/CartItem/index.jsx
@@ -3,20 +3,23 @@ import './CartItem.scss';
 import Counter from './components/Counter';
 import deleteItem from '@images/delete_item.png';
 
-const CartItem = () => {
+const CartItem = ({ index, name, price, quantity, deleteCartItem, changeQuantity }) => {
 	return (
 		<div className="cart-item">
 			<div className="cart-item-info">
 				<img src={cartItemImg} alt="cart item" />
 				<div className="label">
-					<h2>Kristina Dam Oak Table With White Marble Top</h2>
-					<span>$2.195</span>
+					<h2>{name}</h2>
+					<span>{'$' + price}</span>
 				</div>
 			</div>
 			<div className="cart-item-functionality">
-				<Counter />
-				<span>$2.195</span>
-				<button className="delete">
+				<Counter index={index} value={quantity} onChange={changeQuantity} />
+				<span>{'$' + price * quantity}</span>
+				<button
+					className="delete"
+					onClick={() => deleteCartItem(index)}
+				>
 					<img src={deleteItem} />
 				</button>
 			</div>
