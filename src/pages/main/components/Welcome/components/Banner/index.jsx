b70e53c0d5670ee53aa69341b87dd99a0d745200
Taras <tarasmikulskyi@ukr.net>
2023-08-31 18:01:43 +0300

    Added link to facebook in welcome banner

--- src/pages/main/components/Welcome/components/Banner/index.jsx
+++ src/pages/main/components/Welcome/components/Banner/index.jsx
@@ -10,7 +10,7 @@ const Banner = () => {
 				<h3 className='headline'>Follow us on Facebook</h3>
 				<p className='text'>Sed ut perspiciatis unde omnis iste natus error sit</p>
 			</div>
-			<Button className={'banner-sales-button'} text={'follow'} />
+			<a href="https://www.facebook.com"><Button className={'banner-sales-button'} text={'follow'} /></a>
 		</div>
 	)
 }
